60d5469eebd7b48a5d48e24c13b39bf98cb318bc
Paulius G <paulius@2simple.com>
2023-12-16 15:41:49 +0000

    add skills component

---
+++ src/app/components/skills/Skills.tsx
@@ -0,0 +1,68 @@
+
+import styles from './skills.module.css'
+import sharedStyles from '../../styles/shared.module.css'
+
+export default function Skills() {
+  return (
+    <div className={styles.skills}>
+      <h1 className={styles['section-bg-heading']}>
+        My Skills
+      </h1>
+      <h1 className={styles['section-heading']}>
+        Professional Skills
+      </h1>
+      <h3 className={styles['sub-heading']}>My <span>Skills</span></h3>
+      <div className={`${sharedStyles['section-border']} ${styles['section-border']}`}></div>
+      <div className={styles['skills-cards']}>
+        <div className={styles['skills-card']}>
+          <div className={styles['skill']}>
+            <span>HTML</span>
+            <span>95%</span>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, culpa.</p>
+          <div className={styles['skills-card-progress']}></div>
+        </div>
+        <div className={styles['skills-card']}>
+          <div className={styles['skill']}>
+            <span>CSS</span>
+            <span>90%</span>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, culpa.</p>
+          <div className={styles['skills-card-progress']}></div>
+        </div>
+        <div className={styles['skills-card']}>
+          <div className={styles['skill']}>
+            <span>JavaScript</span>
+            <span>85%</span>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, culpa.</p>
+          <div className={styles['skills-card-progress']}></div>
+        </div>
+        <div className={styles['skills-card']}>
+          <div className={styles['skill']}>
+            <span>React.js</span>
+            <span>75%</span>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, culpa.</p>
+          <div className={styles['skills-card-progress']}></div>
+        </div>
+        <div className={styles['skills-card']}>
+          <div className={styles['skill']}>
+            <span>Node.js</span>
+            <span>85%</span>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, culpa.</p>
+          <div className={styles['skills-card-progress']}></div>
+        </div>
+        <div className={styles['skills-card']}>
+          <div className={styles['skill']}>
+            <span>Vue.js</span>
+            <span>75%</span>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, culpa.</p>
+          <div className={styles['skills-card-progress']}></div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css'
 import Navigation from './components/navigation/Navigation'
 import Landing from './components/landing/Landing'
+import Skills from './components/skills/Skills'
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <Navigation></Navigation>
       <Landing></Landing>
+      <Skills></Skills>
 
 
 
