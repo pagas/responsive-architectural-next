fb7bb471c9dff0251f0889b78a6ee51dab907073
Paulius G <paulius@2simple.com>
2023-12-16 09:31:07 +0000

    implement navigation and animation

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -1,16 +1,25 @@
 
+"use client"
+import { useState } from 'react'
 import styles from './navigation.module.css'
+import sharedStyles from '../../styles/shared.module.css'
 
 export default function Navigation() {
+  const [showNav, setShowNav] = useState(false);
+
+  const openNavHandler = () => {
+    setShowNav(state => !state)
+  }
+
   return (
-    <>
-      <div className={styles.menuIcon}>
+    <div className={showNav ? styles['is-opened'] : ''}>
+      <div className={styles['menu-icon']} onClick={openNavHandler}>
         <div className={`${styles.line} ${styles['line-1']}`}></div>
         <div className={`${styles.line} ${styles['line-2']}`}></div>
       </div>
       <div className={styles.navigation}>
-        <div className="section-border"></div>
-        <div className={styles.navItems}>
+        <div className={`${sharedStyles['section-border']} ${styles['section-border']}`}></div>
+        <div className={styles['nav-items']}>
           <a href="#">Home</a>
           <a href="#">About</a>
           <a href="#">Projects</a>
@@ -21,6 +30,6 @@ export default function Navigation() {
           <a href="#">Contacts</a>
         </div>
       </div>
-    </>
+    </div>
   )
 }
