699c39bd0efe16340fd1775d796f1093ace01f8a
Paulius G <paulius@2simple.com>
2023-12-17 15:27:37 +0000

    social media component ability to pass icon style

--- src/app/components/UI/social-media/SocialMedia.tsx
+++ src/app/components/UI/social-media/SocialMedia.tsx
@@ -2,13 +2,17 @@
 import styles from './socialMedia.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
-export default function SocialMedia({ className = '' }: { className?: string }) {
+export default function SocialMedia({
+  iconClassName = ''
+}: {
+  iconClassName: string
+}) {
 
   return (
-    <div className={`${styles['social-media']} ${className}`}>
-      <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
-      <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
-      <FontAwesomeIcon icon={faTwitter} className={styles.icon} />
+    <div className={styles['social-media']}>
+      <FontAwesomeIcon icon={faFacebook} className={`${styles.icon} ${iconClassName}`} />
+      <FontAwesomeIcon icon={faInstagram} className={`${styles.icon} ${iconClassName}`} />
+      <FontAwesomeIcon icon={faTwitter} className={`${styles.icon} ${iconClassName}`} />
     </div>
   )
 }
--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,18 +1,12 @@
 
 import SocialMedia from '../UI/social-media/SocialMedia'
 import styles from './footer.module.css'
-// import Header from '../UI/header/Header';
-// import SubHeader from '../UI/sub-header/SubHeader';
-// import SectionBg from '../UI/section-bg/SectionBg';
-// import SectionBorder from '../UI/section-border/SectionBorder';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faLocationDot, faUser, faEnvelope, faPhone, faAsterisk } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
   return (
     <section className={styles.footer}>
 
-      <SocialMedia className={styles['social-media']} ></SocialMedia>
+      <SocialMedia iconClassName={styles.icon}></SocialMedia>
       <div className={styles.copyright}>
         <p>© Web<span>Dev</span> All Rights Reserved. 2023</p>
       </div>
