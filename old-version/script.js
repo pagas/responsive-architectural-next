50631129390a6a7c2ec8c0407d2799447fb1205d
Paulius G <paulius@2simple.com>
2023-12-18 15:14:14 +0000

    delete files not needed

--- old-version/script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".menu-icon");
-const container = document.querySelector(".container");
-menuIcon.addEventListener("click", function (e) {
-    container.classList.toggle("change");
-});
-
